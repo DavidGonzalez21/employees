9fe935c6fd9dcbacf497196560692d019efae93f
vicente garcia <vbgarciag@gmail.com>
2016-12-02 09:33:07 -0600

    updates

--- resources/views/content/register.blade.php
+++ resources/views/content/register.blade.php
@@ -1,5 +1,16 @@
 @include('headers.header')
-<div class="well">
-  <button type="button" name="button" class="btn btn-danger">Button</button>
+<div class="container content">
+  <img src="https://www.clickittech.com/wp-content/uploads/2015/03/logo_sample8-Converted-e1426278580674.png" alt="" class="img-responsive img-rounded img-logo">
+  {{ Form::open(array('action' => 'RegisterController@create', 'method' => 'POST')) }}
+    <div class="input-control div-form-rg well">
+      <input type="text" name="first_name" value="" class="form-control text-capitalize" placeholder="First Name"><br>
+      <input type="text" name="last_name" value="" class="form-control text-capitalize" placeholder="Last Name"><br>
+      <input type="text" name="middle_name" value="" class="form-control text-capitalize" placeholder="Middle Name"><br>
+      <input type="text" name="email" value="" class="form-control text-capitalize" placeholder="Email"><br>
+      <input type="password" name="password" value="" class="form-control" placeholder="Password"><br>
+      <input type="text" name="cell_phone" value="" class="form-control text-capitalize" placeholder="Cell Phone"><br>
+      <button type="submit" name="button" class="btn btn-primary" style="width: 100%;">Register Now</button>
+    </div>
+  {{ Form::close() }}
 </div>
 @include('headers.footer')
--- resources/views/headers/header.blade.php
+++ resources/views/headers/header.blade.php
@@ -2,13 +2,10 @@
 <html>
 <head>
   <title>Laravel</title>
-  <!--<script src="/js/jquery-1.10.2.js"></script>-->
   <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="css/app.css">
 
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="/js/app.js"></script>
 </head>
 <body>
